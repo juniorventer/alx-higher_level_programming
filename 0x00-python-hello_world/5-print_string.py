d006563b5d166fc19df698df09f592c8f8e5d9a8
juniorventer <roccovent12@gmail.com>
2022-09-21 15:34:37 -0700

    Python Hello World

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print( 3 * str ) # prints the value of str 3 times
-print( str[0:9] ) # slice and print up to the character in the 9 index
+print(3 * str)
+print(str[:9])
