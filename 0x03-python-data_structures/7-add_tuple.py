39c8554d047c7e59e9ff4c01ec0ab693cc7ec639
juniorventer <roccovent12@gmail.com>
2022-09-26 16:44:57 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = validate_tuple(tuple_a)
-    tuple_b = validate_tuple(tuple_b)
-
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
-
-def validate_tuple(_tuple=()):
-    if len(_tuple) < 2:
-        if len(_tuple) == 1:
-            _tuple = (_tuple[0], 0)
-        elif len(_tuple) == 0:
-        _tuple = (0, 0)
-    elif len(_tuple) > 2:
-        _tuple = (_tuple[0], _tuple[1])
-
-    return _tuple
+    """Adds elements of 2 tuples"""
+    if len(tuple_a) < 1:
+        tuple_a = (0, 0)
+    elif len(tuple_a) < 2:
+        tuple_a = (tuple_a[0], 0)
+    if len(tuple_b) < 1:
+        tuple_b = (0, 0)
+    elif len(tuple_b) < 2:
+        tuple_b = (tuple_b[0], 0)
+    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return res
