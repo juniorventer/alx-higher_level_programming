c396a34824f3090fb0269504bd2cbc0ca2ab564e
juniorventer <roccovent12@gmail.com>
2022-09-26 17:53:36 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,24 +13,24 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *start = NULL, *end = NULL;
 	unsigned int i = 0, len = 0, len_cyc = 0, len_list = 0;
-	
+
 	if (head == NULL)
 		return (0);
-	
+
 	if (*head == NULL)
 		return (1);
-	
+
 	start = *head;
 	len = listint_len(start);
 	len_cyc = len * 2;
 	len_list = len_cyc - 2;
 	end = *head;
-	
+
 	for (; i < len_cyc; i = i + 2)
 	{
 		if (start[i].n != end[len_list].n)
 			return (0);
-		
+
 		len_list = len_list - 2;
 	}
 	return (1);
@@ -48,7 +48,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *current = head;
 	unsigned int iter_times = 0;
-	
+
 	if (head)
 	{
 		while (current != NULL)
@@ -73,7 +73,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 size_t listint_len(const listint_t *h)
 {
 	int lenght = 0;
-	
+
 	while (h != NULL)
 	{
 		++lenght;
