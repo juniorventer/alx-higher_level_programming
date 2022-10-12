4310f1c60c493e6c4e59545d53d00670bada9600
junior-venter <roccovent12@gmail.com>
2022-10-11 20:08:51 -0700

    Python - Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class Square """
+"""class Square"""
 
 
 class Square:
@@ -7,7 +7,7 @@ class Square:
     def __init__(self, size=0):
         """square initialization"""
         self.size = size
-    
+
     @property
     def size(self):
         """returns current size of the square"""
@@ -15,8 +15,8 @@ class Square:
 
     @size.setter
     def size(self, value):
-         """validating size"""
-        if type(value) != int:
+        """validating size"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
@@ -32,5 +32,5 @@ class Square:
         for x in range(0, self.__size):
             [print("#", end="") for y in range(self.__size)]
             print("")
-        if self.__size == 0:
-            print("")
+            if self.__size == 0:
+                print("")
