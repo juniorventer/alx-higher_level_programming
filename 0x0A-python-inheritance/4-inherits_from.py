7da8bd3b6f96c980a642e389170cb03e07f8bf55
junior-venter <roccovent12@gmail.com>
2022-10-24 05:52:01 -0700

    Python - Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-def inherits_from(obj, a_class):
-    if isinstance(obj, a_class) and \
-            issubclass(a_class, obj.__class__) is False:
-            return True
+"""
+Module for is_same_class method
+"""
 
+
+def inherits_from(obj, a_class):
+    """Method for comparing object classes
+
+    Args:
+        obj (unknown): object whose type is to be checked
+        a_class (str): class criteria to validate
+
+    Return:
+        True if obj isinstance of a_class/ class that inherits from it
+        otherwise False
+
+    """
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
     return False
