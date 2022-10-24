f104b79b64c9465dd92bf19311c612cd5bddebd6
junior-venter <roccovent12@gmail.com>
2022-10-24 08:14:02 -0700

    Python - Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,22 +1,10 @@
 #!/usr/bin/python3
-"""
-Module for is_same_class method
-"""
+"""Check if object is instance of class or subclass"""
 
 
-def is_same_class(obj, a_class):
-    """Method for comparing object classes
-
-    Args:
-        obj (unknown): object whose type is to be checked.
-        a_class (str): class criteria to validate.
-
-    Return:
-        True if obj isinstance of a_class/ class that inherits from it.
-        otherwise False
-
-    """
-
-    if issubclass(type(obj), a_class) or isinstance(obj, a_class):
+def is_kind_of_class(obj, a_class):
+    """return true is isinstance is true else false"""
+    x = isinstance(obj, a_class)
+    if x:
         return True
     return False
