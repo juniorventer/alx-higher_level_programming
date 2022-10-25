cf0515c108c99cacba7cf501e9ce146e62ef810d
junior-venter <roccovent12@gmail.com>
2022-10-25 04:31:33 -0700

    Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list and save them to file"""
+"""from json import dumps"""
+
+
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+
+
+def save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
