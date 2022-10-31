7b4f48ac92fd179c0d9bf7d09d6cbefb9664819e
junior-venter <roccovent12@gmail.com>
2022-10-31 16:04:59 -0700

    Python - Almost a circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -34,11 +34,10 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base_instance.id, 115)
         base_instance = Base(67)
         self.assertEqual(base_instance.id, 67)
-    
+
     def test_id_as_negative(self):
         """
         Test for a negative Base Class id
-        
         """
         base_instance = Base(-91)
         self.assertEqual(base_instance.id, -91)
