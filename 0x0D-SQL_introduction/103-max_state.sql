db644856a377f598466c2afc84a800e53669f5ea
junior-venter <roccovent12@gmail.com>
2022-11-29 02:33:01 -0800

    SQL - Introduction

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,6 @@
--- Converts `hbtn_0c_0` database to UTF8 in MySQL Server.
-
-USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Displays the max temperature of each state from the temperatures table
+SELECT state,
+	MAX(value) AS "max_temp"
+	FROM temperatures
+	GROUP BY state
+	ORDER BY state;
