a6dc4aecef3eae43c70dc251e2cdde7fadb19d64
junior-venter <roccovent12@gmail.com>
2023-01-17 03:11:28 -0800

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-"""
+"""Script that fetches from a url using urllib package"""
 
-from urllib.request import Request, urlopen
+import urllib.request as request
 
 
 if __name__ == "__main__":
     req = Request('https://intranet.hbtn.io/status')
 
-    with urlopen(req) as res:
-        content = res.read()
-        utf8_content = content.decode('utf-8')
-
-        print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html + r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf8')))
