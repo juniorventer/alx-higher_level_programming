a9d34d5b014738a2acd252c81ee54356dd41fa4b
junior-venter <roccovent12@gmail.com>
2023-01-17 06:43:30 -0800

    Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,7 @@ import requests
 
 if __name__ == '__main__':
     response = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-            .format(type(response.text), response.text))
+
+    print('Body response:')
+    print('\t- type: {_type}'.format(_type=type(req.text)))
+    print('\t- content: {_content}'.format(_content=req.text))
