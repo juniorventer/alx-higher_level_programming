49a7783d0587cb485b31b61381d0af9932c1a2b4
junior-venter <roccovent12@gmail.com>
2023-01-17 03:38:24 -0800

    Python - Network #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email address, sends a
-POST request to the passed URL with the email
-as a parameter, and finally displays the body
-of the response.
+"""a Python script that takes in a URL and an email address, sends a
+POST request to the passed URL with the email as a parameter, and 
+finally displays the body of the response.
 """
 
 import requests
