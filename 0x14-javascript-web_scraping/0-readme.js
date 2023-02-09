f2923c1bf95b20206ce6a85178edc9b9340186ca
junior-venter <roccovent12@gmail.com>
2023-02-09 14:53:14 -0800

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
+//The first argument is the file path
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', function (err, data) 
- if (err) {
-	console.log(err);
- } else {
-	process.stdout.write(data);
- }
+// The content of the file must be written in utf-8
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		process.stdout.write(data);
+	}
 });
